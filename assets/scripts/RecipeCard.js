b388905a699515db019e881f0c8db74b65f24278
mazen-hamdi <mhamdi@ucsd.edu>
2025-05-13 15:45:23 -0700

    add style to recipecard.js

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -12,6 +12,7 @@ class RecipeCard extends HTMLElement {
 		// A2. TODO - Create an <article> element - This will hold our markup once our data is set
 		 const article = document.createElement('article');
 		// A3. TODO - Create a style element - This will hold all of the styles for the Web Component
+		const style = document.createElement('style');
 		// A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made (copy everything INSIDE the <style> tag>)
 		style.textContent = `
   * {
